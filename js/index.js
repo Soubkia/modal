ec14ce0af12a2693e9c108ef0ce20cf4fbe67c7a
Soubkia <soubkia@gmail.com>
2015-02-13 21:03:35 -0500

    rename before trying custom tags

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 //Fork of http://codepen.io/jeffmccarthyesq/pen/LEEKLZ
 //Now displays content that extends past the wrap size
 $('button').on('click', function(){
-  $('.wrap, button').toggleClass('active');
-  
-  return false;
+  $('.modal, button').toggleClass('active');
 });
